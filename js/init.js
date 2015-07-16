0f7fa918212b156b94b5d14aa770115c37b8e9d4
Armand Niampa <armand.niampa@gmail.com>
2015-07-16 20:50:09 +0200

    Rename files.

--- js/init.js
+++ js/init.js
@@ -7,5 +7,9 @@
 
     new WOW().init();
 
+    $('body > .overlay').fadeIn().delay(2000).queue(function(){
+        $(this).addClass("slideOutLeft").dequeue();
+    });
+    // .addClass('slideOutLeft');
   }); // end of document ready
 })(jQuery); // end of jQuery name space
